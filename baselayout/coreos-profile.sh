e880fb0383915749d351d67e720c3124dc67ba0d
Euan Kemp <euan.kemp@coreos.com>
2017-08-02 18:39:04 -0700

    coreos-profile: Show update status using conf file
    This coordinator conf file is being introduced in locksmith and the
    Container Linux Update Operator.
    
    This allows the profile to correctly display information about the
    update strategy regardless whether it's being managed by locksmithd or
    cluo.
    
    See https://github.com/coreos/container-linux-update-operator/issues/88
    
    This retains the same message for the case of a coordinator not running.
    
    It adds additional messages for other statuses, such as "disabled" and
    "reboot planned".
    These messages are highlighted in yellow.

--- baselayout/coreos-profile.sh
+++ baselayout/coreos-profile.sh
@@ -2,8 +2,34 @@
 
 # Only print for interactive shells.
 if [[ $- == *i* ]]; then
-	if ! systemctl is-active locksmithd > /dev/null; then
+	if ! [ -f  /run/update-engine/coordinator.conf ]; then
+		# No coordinator running
 		echo -e "Update Strategy: \033[31mNo Reboots\033[39m"
+	elif flock --nonblock --exclusive /run/update-engine/coordinator.conf --command /bin/true &>/dev/null; then
+		# Coordinator ran, but isn't currently running
+		echo -e "Update Strategy: \033[31mNo Reboots\033[39m"
+	else
+		# subshell to avoid env pollution
+		(
+			# Helper warn if disabled or rebooting soon
+			warn() {
+					echo -e "Update Status: \033[33m${NAME} - ${1}\033[39m"
+			}
+			source /run/update-engine/coordinator.conf
+			case "${STATE}" in
+				running|starting)
+					;;
+				disabled)
+					warn "Updates disabled"
+					;;
+				rebooting|reboot-planned)
+					warn "Reboot planned"
+					;;
+				*)
+					warn "${STATE}"
+					;;
+			esac
+		)
 	fi
 
 	FAILED=$(systemctl list-units --state=failed --no-legend)
